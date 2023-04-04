faa40797081403e924a70aaff61dfbad7f5359d1
Alana Auerbach Carsten <alanacarsten@gmail.com>
2023-04-04 17:57:09 -0300

    ajuste para gitpages

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,7 +5,7 @@ import FormVehicles from '@/views/FormVehicles.vue'
 import { createRouter, createWebHistory  } from 'vue-router'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL + 'carango-bom'),
   routes: [
     {
       path: '/',
