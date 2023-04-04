a7f21d9084c2c880f01841a58cc0d826fa3748ee
Alana Auerbach Carsten <alanacarsten@gmail.com>
2023-04-04 19:00:24 -0300

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,24 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  root: path.join(__dirname, "src"),
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    emptyOutDir: true,
+    outDir: path.resolve(__dirname, 'dist'),
+    rollupOptions: {
+      input: {
+        index: path.resolve(__dirname, "src/index.html")        
+      }
     }
   }
 })
